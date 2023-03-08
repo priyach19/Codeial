72f1e212562ba804259a0cf450135041bd9ce823
Priya Chaudhary <priyachuadhary1909@gmail.com>
2023-03-09 00:25:02 +0530

    create another controller

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router= express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
-//router.use('/users',require('./users'));
+router.use('/users',require('./users'));
+router.get('/posts',require('./posts'));
 
-console.log('inside router');
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-// const express=require('express');
-// const router=express.Router();
+const express=require('express');
+const router=express.Router();
 
-// const usersController=require('../controllers/users_controller');
-// router.get('/profile',usersController.profile);
+const usersController=require('../controllers/users_controller');
+router.get('/profile',usersController.profile);
+
+
+
+module.exports=router;
