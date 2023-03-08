0e1cf6b8f0c4d81568f4a8d8b877664ef0586c8d
Priya Chaudhary <priyachuadhary1909@gmail.com>
2023-03-08 23:45:46 +0530

    create a controller for home routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router= express.Router();
+
+const homeController=require('../controllers/home_controller');
+
+router.get('/',homeController.home);
+//router.use('/users',require('./users'));
+
+console.log('inside router');
+
+
+
+module.exports=router;
